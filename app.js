a02f94cbd6c1a29203249293226006619e5f9367
Stevie Lindsey Jr <stevie.lindsey.jr@gmail.com>
2024-12-25 18:03:02 -0600

    creat4ed the files I will be using, and started putting buttons on the dom

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+
+function clickButton () {
+    let buttonId = document.querySelector(".numberButton").id
+    console.log("You're clicking a button", buttonId);
+
+    
+}
